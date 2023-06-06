09c07317dbf74a2c18c1b9eedc57338934c029b1
Lays Almeida <laysnanda2012@gmail.com>
2023-06-05 23:56:08 -0300

    add files

---
+++ app/src/main/java/com/example/empregaeu4/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.empregaeu4
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.empregaeu4.databinding.ActivityMainBinding
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val edNome = binding.edUsuario
+        val btLogin = binding.btLogin
+        btLogin.setOnClickListener {
+            if(edNome.text.toString()=="Lays Almeida") {
+                val inten = Intent(this, CadastroVagaActivity::class.java)
+                startActivity(inten)
+                val database = Firebase.database
+                val myRef = database.getReference("teste")
+                myRef.setValue("Hello, World!")
+
+            }
+            else{
+                val inten = Intent(this, ListaVagasActivity::class.java)
+                startActivity(inten)
+
+            }
+        }
+        //setContentView(R.layout.activity_main)
+    }
+}
